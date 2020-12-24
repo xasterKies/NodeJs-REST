62747c96cdc25f66494ee468f6638a4c6adda522
TiokengSamuel <samueledward40@gmail.com>
2020-12-24 08:42:25 +0100

    listening on port

--- server.js
+++ server.js
@@ -3,3 +3,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     console.log('reques made like so');
 });
+
+server.listen(3000, 'localhost', () => {
+    console.log('listening for request on port 3000')
+})
