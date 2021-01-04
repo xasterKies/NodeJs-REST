da5ff87c684096ac22fafada5baf7f8688d7f711
TiokengSamuel <samueledward40@gmail.com>
2021-01-04 21:59:31 +0100

    view engine

--- app.js
+++ app.js
@@ -5,14 +5,15 @@ const app = express();
 
 //listen for requests
 app.listen(3000);
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
 
-    res.send('<p>Home page</p>');
+    //res.send('<p>Home page</p>');
 });
 
 
 app.get('/about', (req, res) => {
 
-    res.send('<p>about page</p>');
+    //res.send('<p>about page</p>');
 });
