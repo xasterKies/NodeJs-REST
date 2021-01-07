c0291dccb6396ee34bb380f40e95ab3a61bc7bff
TiokengSamuel <samueledward40@gmail.com>
2021-01-07 02:51:35 +0100

    next key word

--- app.js
+++ app.js
@@ -7,11 +7,12 @@ const app = express();
 app.listen(3000);
 app.set('view engine', 'ejs');
 
-app.use((req, res) => {
+app.use((req, res, next) => {
     console.log('new request made: ');
     console.log('host: ', req.hostname);
     console.log('path: ', req.path);
     console.log('method: ', req.method);
+    next();
 })
 
 app.get('/', (req, res) => {
